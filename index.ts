5c4d372c460ff14081fcd6d2cd37ccb3d0c79451
7rancesco <fcfrancescocrupi@gmail.com>
2023-04-05 08:53:04 +0200

    add compare function

--- index.ts
+++ index.ts
@@ -3,21 +3,40 @@ export function getSearch (
     array : object[],
     properties : string[] | null
 ) {
-
+    const output : object[] = [];
     array.forEach( obj => {
         if(properties !== null){
             //search in specific properties
             properties.forEach( prop => {
-                console.log(obj[prop])
+                if(compare(obj[prop], input))
+                output.push(obj[prop])
             });
         } else {
             //search in each properties
             const objKeys = Object.keys(obj);
             if(objKeys){
                 objKeys.forEach(key => {
-                    console.log(obj[key])
+                    if(compare(obj[key], input))
+                    output.push(obj[key])
                 });
             }
         }
     });
+    return output
+}
+
+function compare( a : any, b : string ){
+    let prop = a;
+    let input = b;
+    const isNotString = typeof(a) !== 'string';
+    if(isNotString){
+        prop.toString();
+    }
+    prop.toLowerCase();
+    input.toLowerCase();
+
+    if(prop === input){//LIKE
+        return true
+    }
+    return false
 }
