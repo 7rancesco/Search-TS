e8064c5a35ea5e76188e782ca2c3a108be3bb798
7rancesco <fcfrancescocrupi@gmail.com>
2023-04-05 08:21:21 +0200

    add gitignore

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
-export function printMsg ( input : string ) {
-    alert( 'Hello npm by ' + input )
+export function getSearch ( 
+    input : string,
+    array : object[],
+    properties : string[] | null
+) {
+
+    array.forEach( obj => {
+        if(properties !== null){
+            //search in specific properties
+            properties.forEach( prop => {
+                console.log(obj[prop])
+            });
+        } else {
+            //search in each properties
+            const objKeys = Object.keys(obj);
+            if(objKeys){
+                objKeys.forEach(key => {
+                    console.log(obj[key])
+                });
+            }
+        }
+    });
 }
